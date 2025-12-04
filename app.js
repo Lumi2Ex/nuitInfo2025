000e65696893af2e33b3fa6f2a23ab2283990f69
Lumi <stellargames.lumi@gmail.com>
2025-12-04 18:12:04 +0100

    audio visualize

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import WaveSurfer from "https://cdn.jsdelivr.net/npm/wavesurfer.js@7/dist/wavesurfer.esm.js";
+import Spectrogram from "https://cdn.jsdelivr.net/npm/wavesurfer.js@7/plugins/spectrogram.esm.js";
 
 const surfer = WaveSurfer.create({
   container: "#waveform",
   waveColor: "violet",
   progressColor: "purple",
+  // enable WebAudio analyzer
+  autoCenter: true,
 });
 
-// MUST be a correct path
+// Load audio
 surfer.load("./resources/whistle.mp3");
 
+// Add a spectrogram visualizer
+Spectrogram.create({
+  wavesurfer: surfer,
+  container: "#visualizer",
+  labels: true,
+});
+    
 document.getElementById("playBtn").onclick = () => {
   surfer.playPause();
 };
